051a3ac51e3d32926d968b143a4326711662e567
odirley pereira <odirleyrp@gmail.com>
2026-03-16 21:48:19 -0300

    ajuste

---
+++ projetos_backstage/meu_projeto_flask/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+import datetime
+import socket
+
+app = Flask(__name__)
+
+
+@app.route('/api/v1/details', methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+
+
+        return jsonify({
+            'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+            'hostname': socket.gethostname()
+            })
+
+
+@app.route('/api/v1/healthz', methods = ['GET', 'POST'])
+def health():
+    if(request.method == 'GET'):
+
+        return jsonify({
+            'status': 'up'
+
+
+            }), 200
+
+@app.route('/home/<int:num>', methods = ['GET'])
+def disp(num):
+
+    return jsonify({'data': num**2})
+
+
+# driver function
+if __name__ == '__main__':
+
+    app.run(debug = True)
