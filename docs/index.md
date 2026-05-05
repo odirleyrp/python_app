c8311ae4abf64f26f055f106e016d50ad0c18c0b
odirley pereira <odirleyrp@gmail.com>
2026-05-04 22:43:16 -0300

    ajuste

--- docs/index.md
+++ docs/index.md
@@ -1,10 +1,20 @@
-# Documents for python-app
+# Documents for ${{values.app_name}}
 
-This application has two endpoints
-- '/api/v1/info'
-- '/api/v1/heathz'
+This is a template app!
 
-Here you could expand on what each these endpoints do.
+For fun, it shows you a random dev excuse + a random cat
 
 # How to access the app?
-You can access the app by accesssing this URL: '(http://app.bora.local:8888/api/v1/healthz)'
+
+You can access the app by accessing this URL: `${{values.app_name}}-${{values.app_env}}.test.com` 
+
+# Extra info?
+
+This application has two API endpoints:
+
+- `/api/v1/info`
+- `/api/v1/healthz`
+
+Here you could expand on what each of these endpoints do.
+
+
--- src/app.py
+++ src/app.py
@@ -1,39 +1,55 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, send_from_directory
 import datetime
 import socket
+import random
+
+
+dev_excuses = [
+    "It worked on my machine.",
+    "I thought I fixed that.",
+    "That's just a warning, not an error.",
+    "You must have a corrupted database.",
+    "It was working yesterday.",
+    "I didn't write that part of the code.",
+    "That's a hardware problem.",
+    "I can't reproduce the problem.",
+    "The client must have done something wrong.",
+    "I have never seen that before."
+]
 
 app = Flask(__name__)
 
 
-@app.route('/api/v1/details', methods = ['GET', 'POST'])
+@app.route('/')
+
 def home():
-    if(request.method == 'GET'):
+    return render_template(
+        'index.html',
+        cat_img=f"images-front/cat{random.randint(1, 7)}.gif",
+        dev_excuse=random.choice(dev_excuses)
+    )
 
+@app.route('/api/v1/info')
 
-        return jsonify({
-            'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
-            'hostname': socket.gethostname(),
-            'message': 'teste de versao python gitHub action pipeline v3.42A'
-            })
+def info():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'deployed_on': 'kubernetes',
+    })
 
+@app.route('/api/v1/healthz')
 
-@app.route('/api/v1/healthz', methods = ['GET', 'POST'])
 def health():
-    if(request.method == 'GET'):
-
-        return jsonify({
-            'status': 'up'
+    # Do an actual check here
+    return jsonify({'status': 'up'}), 200
 
 
-            }), 200
+@app.route('/images-front/<filename>')
+def images_frontend(filename):
+    return send_from_directory('templates/img', filename)
 
-@app.route('/home/<int:num>', methods = ['GET'])
-def disp(num):
-
-    return jsonify({'data': num**2})
-
-
-# driver function
 if __name__ == '__main__':
 
     app.run(host="0.0.0.0")
+
